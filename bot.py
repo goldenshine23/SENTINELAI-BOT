98816bc36bd6008862df7d916b39de0c963b40fd
Emenike Chinedu <chinedudazi@gmail.com>
2025-08-01 12:26:38 -0700

    Merge remote main into local main

--- bot.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from metaapi_connector import init_metaapi, metaapi
-
-async def main():
-    await init_metaapi()
-    # continue your strategy setup here...
-
-if __name__ == '__main__':
-    asyncio.run(main())
